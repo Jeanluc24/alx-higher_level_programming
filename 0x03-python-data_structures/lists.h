bdb24b5bc6c4531eba55a5d0b21320061be2278c
Jeanluc24 <jnlucmusengayezu24@gmail.com>
2024-03-18 14:10:35 -0700

    Linked list palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -11,8 +11,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +21,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* end of LISTS_H file */
+#endif /* LISTS_H */
